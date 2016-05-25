d58d67ec75723e4285c568246b80f22b80869fa3
Willy DONGMO <w.dongmo@gmail.com>
2016-05-25 15:18:02 +0200

    chore

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,6 +7,9 @@ var path    = require('path');
 var yargs   = require('yargs').argv;
 var tpl     = require('gulp-template');
 var rename  = require('gulp-rename');
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname+'/dist'));
 
 /*
 map of paths for using with the tasks below
@@ -33,12 +36,13 @@ gulp.task('todo', function() {
     .pipe(todo({silent: false, verbose: true}));
 });
 
+
 gulp.task('build', ['todo'], function() {
   return gulp.src(paths.entry)
     .pipe(webpack(require('./webpack.config')))
     .pipe(gulp.dest(paths.dest));
 });
-
+/*
 gulp.task('serve', function() {
   browser({
     port: process.env.PORT || 4500,
@@ -49,7 +53,16 @@ gulp.task('serve', function() {
     }
   });
 });
+*/
 
+/**
+* Serving with express instead of browser
+**/
+gulp.task('serve', function(){
+  app.listen(process.env.PORT||4500, function(){
+    console.log('application is listening on port ', process.env.PORT||4500);
+  });
+});
 /*
 simple task to copy over needed files to dist
  */
