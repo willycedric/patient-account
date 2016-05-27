4bcf66044447d8d5f209eafdd4a9ec09548e22ea
Willy DONGMO <w.dongmo@gmail.com>
2016-05-27 18:24:42 +0200

    chore

--- client/app/components/web/web.controller.js
+++ client/app/components/web/web.controller.js
@@ -15,24 +15,44 @@ class WebController {
           user={};
           isValid.$setPristine();
           
-       };   
+       };
+       //delete a user
+       this.deleteUser = (data)=>{
+       		this.http.delete('http://192.168.3.208:3000/api/users',{params:{id:data._id}})
+       		.then((response)=>{
+    			console.log(JSON.stringify(response.data)," was successfully removed");
+    			this.getUsers();
+    		},(err)=>{
+    			console.error(err);
+    		});
+       };
    }
-
+   	//get All the users
     getUsers(){
     	this.http({
     		method:'GET',
-    		url:'http://localhost:3000/api/users'
+    		url:'http://192.168.3.208:3000/api/users'
     	}).then((reponse)=>{
     		this.users = reponse.data;
+    		// Compute the number of row(s) used to display the user
+    		if(this.users.length <= 4){
+    			this.nbRows = 1;
+    		}
+	   		else if(Math.ceil(this.users.length/4) < 4){
+	   			this.nbRows = Math.floor(this.users.length/4)+1;
+	   		}else{
+	   			this.nbRows = Math.floor(this.users.length/4);
+	   		}
     	}, (err)=>{
     		console.error(err);
     	});
     }
 
+    //Add a new user
     postUser(data){
       this.http({
         method:'POST',
-        url:'http://localhost:3000/api/users',
+        url:'http://192.168.3.208:3000/api/users',
         data:data
       }).then((response)=>{
         this.newUser = response.data;
